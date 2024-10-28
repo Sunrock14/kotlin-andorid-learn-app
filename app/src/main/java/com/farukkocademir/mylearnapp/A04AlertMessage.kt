f313e79d3ccccea7ce07841a71fafcca21208aaf
Sunrock14 <farukkocademir@hotmail.com>
2024-10-28 10:15:02 +0300

    alert messages

--- app/src/main/java/com/farukkocademir/mylearnapp/A04AlertMessage.kt
+++ app/src/main/java/com/farukkocademir/mylearnapp/A04AlertMessage.kt
@@ -1,20 +1,51 @@
 package com.farukkocademir.mylearnapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.farukkocademir.mylearnapp.databinding.ActivityA01PersonBinding
+import com.farukkocademir.mylearnapp.databinding.ActivityA04AlertMessageBinding
 
 class A04AlertMessage : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityA04AlertMessageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_a04_alert_message)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityA04AlertMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    // Üç farklı mesaj gösterme yöntemi vardır
+    // 1. Toast Mesajı
+    // 2. AlertDialog Mesajı
+    // 3. Snackbar Mesajı
+
+    // Context nedir? : Uygulamanın nerede çalıştığı takip etmek için kullanılır.
+    // Context uygulamanın hangi aktivitede olduğunu gösterir.
+    // Activity Context => this : Genelde activity context kullanılır.
+    // Application Context : Uygulamanın kendisini gösterir.
+
+    fun ShowToastMesssage(view : View){
+        Toast.makeText(this,"Button Tıklandı", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this,"Button Tıklandı", Toast.LENGTH_LONG).show()
+    }
+    fun ShowAlertDialog(view : View){
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Başlık")
+        alert.setMessage("Mesaj")
+        alert.setPositiveButton("Tamam"){dialog, which ->
+            Toast.makeText(this,"Tamam seçildi",Toast.LENGTH_LONG).show()
         }
+        alert.setNegativeButton("İptal"){dialog, which ->
+            Toast.makeText(this,"İptal seçildi",Toast.LENGTH_LONG).show()
+        }
+        alert.show()
+
     }
 }
